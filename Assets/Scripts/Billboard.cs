d258bdc0f29552da64076d1f6187c978b3b77d0b
ShatnersActin <97978762+ShatnersActin@users.noreply.github.com>
2022-02-17 18:48:48 -0500

    Billboard

---
+++ Assets/Scripts/Billboard.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Billboard : MonoBehaviour
+{
+    public Camera m_Camera;
+
+    private void LateUpdate()
+    {
+        m_Camera = Camera.main;
+
+        transform.LookAt(transform.position + m_Camera.transform.rotation * Vector3.forward, m_Camera.transform.rotation * Vector3.up);
+    }
+}
